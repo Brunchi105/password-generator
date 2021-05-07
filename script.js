4010a27a0c3926332f21ed7f9fcba698a730957c
Bruno Aguirre <bruno_aguirrejr@yahoo.com>
2021-05-06 22:55:15 -0700

    added comments

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//Variable arrays
 var lowerCaseAlphabet = "abcdefghijklmnopqrstuvwxyz";
 var upperCaseAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var numbers = "0123456789";
@@ -9,34 +10,36 @@ var generatePassword = function () {
   var passArr = [];
   var passLength = prompt("Chose a password length between 8 and 128 characters");
 
+// Checks if length of password chose meets the requirement 
   if (passLength >= 8 && passLength <= 128) {
-    var isLowerCase = confirm("Use lower case letters?");
+    var isLowerCase = confirm("Use lowercase letters?");
 
-    var isUpperCase = confirm("Use upper case letters?");
+    var isUpperCase = confirm("Use uppercase letters?");
 
     var isSpecialChar = confirm("Use special characters?");
 
     var isNumberChar = confirm("Use numbers?");
-
+  // Checks if at least 1 character type was selected
     if (isLowerCase || isUpperCase || isSpecialChar || isNumberChar) {
+  // Checks if lowercase letters will be included in password - pushes characters to array of potential included characters 
       if (isLowerCase) {
         for (var i = 0; i < lowerCaseAlphabet.length; i++) {
           pooledCharacters.push(lowerCaseAlphabet[i]);
         }
       }
-
+  // Checks if uppercase letters will be included in password - pushes characters to array of potential included characters
       if (isUpperCase) {
         for (var i = 0; i < upperCaseAlphabet.length; i++) {
           pooledCharacters.push(upperCaseAlphabet[i]);
         }
       }
-
+  // Checks if special characters will be included in password - pushes characters to array of potential included characters
       if (isSpecialChar) {
         for (var i = 0; i < symbolCharacters.length; i++) {
           pooledCharacters.push(symbolCharacters[i]);
         }
       }
-
+  // Checks if numbers will be included in password - pushes characters to array of potential included characters
       if (isNumberChar) {
         for (var i = 0; i < numbers.length; i++) {
           pooledCharacters.push(numbers[i]);
@@ -52,12 +55,12 @@ var generatePassword = function () {
       return joinedPass;
     } else {
       alert("You need to chose at least 1 type of character");
-
-      return generatePassword();
+      // Function will stop executing and return to initial prompt
+      return generatePassword();  
     }
   } else {
     alert("Incorrect length of characters chosen. Try again.");
-
+    // Function will stop executing and return to initial prompt
     return generatePassword();
   }
 };
